9ae107a06cb31f2d8058ff21e1aa1788737dd2ac
RoqueCorrales <roke12chacon@hotmail.com>
2017-02-09 20:33:52 -0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace LSP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbxSaludo = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApellidos = new System.Windows.Forms.TextBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Saludo";
+            // 
+            // cbxSaludo
+            // 
+            this.cbxSaludo.FormattingEnabled = true;
+            this.cbxSaludo.Items.AddRange(new object[] {
+            "Ingles",
+            "Español"});
+            this.cbxSaludo.Location = new System.Drawing.Point(67, 104);
+            this.cbxSaludo.Name = "cbxSaludo";
+            this.cbxSaludo.Size = new System.Drawing.Size(137, 21);
+            this.cbxSaludo.TabIndex = 1;
+            this.cbxSaludo.SelectedIndexChanged += new System.EventHandler(this.cbxSaludo_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Persona";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Nombre";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 66);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Apellidos";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(67, 32);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(137, 20);
+            this.txtNombre.TabIndex = 5;
+            // 
+            // txtApellidos
+            // 
+            this.txtApellidos.Location = new System.Drawing.Point(67, 63);
+            this.txtApellidos.Name = "txtApellidos";
+            this.txtApellidos.Size = new System.Drawing.Size(137, 20);
+            this.txtApellidos.TabIndex = 6;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(129, 141);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 7;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(12, 170);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(0, 13);
+            this.lblResultado.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(248, 259);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.txtApellidos);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbxSaludo);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Saludar";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbxSaludo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtApellidos;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+
+namespace LSP
+{
+    public partial class Form1 : Form
+    {
+        String s;
+        public Form1()
+        {
+            InitializeComponent();
+            s = "";
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+
+            cargar();
+
+
+        }
+
+        private void cbxSaludo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            s = cbxSaludo.SelectedItem.ToString();
+
+        }
+        private IEnumerable<Type> types()
+        {
+            return AppDomain.CurrentDomain.GetAssemblies()
+                    .SelectMany(s => s.GetTypes())
+                    .Where(p => typeof(Persona).IsAssignableFrom(p))
+                    .Where(a => !a.FullName.Equals("LSP.Persona"));
+        }
+
+        public void cargar()
+        {
+
+            var types = this.types();
+            foreach (var type in types)
+            {
+                Persona p = this.getIntance(type);
+                if (p.idioma().Equals(s))
+                {
+                    lblResultado.Text = txtNombre.Text +" "+txtApellidos.Text +"\n"
+                        +"Saludo: "+ p.saludar();
+                }
+
+
+            }
+        }
+
+            private Persona getIntance(Type type)
+        {
+            return (Persona)Activator.CreateInstance(type);
+        }
+        }
+    }
+
